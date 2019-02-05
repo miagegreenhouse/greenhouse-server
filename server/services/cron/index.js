20df72ccbeabdbe4c7e764c6c214583d629c4c76
LucasFreyss <lfreyss_hp@hotmail.fr>
2019-02-05 23:16:02 +0100

    Connection to MyFood API and beginning of data-merging

--- server/services/cron/index.js
+++ server/services/cron/index.js
@@ -2,6 +2,9 @@ const influxProvider = require('../../influxProvider');
 const config = require('../../config/index');
 const cron = require('node-cron');
 const request = require('request');
+const logger = require('../../logger');
+const moment = require('moment');
+
 const SensorsDataController = require('../../entities/sensorsdata/controller');
 
 const DataSourceEnum = Object.freeze({myfood:1, influxdb:2})
@@ -35,7 +38,6 @@ function influxTaskCron(mongoDb) {
 
 function myFoodTaskCron(mongoDb) {
   // Retrieve last value timestamp
-  console.log('myFoodTaskCron')
   const sensorsdata = new SensorsDataController(mongoDb);
   sensorsdata.getLastTimeStamp().then((timestamp) => {
     // Request data since last timestamp value
@@ -49,16 +51,19 @@ function myFoodTaskCron(mongoDb) {
           'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8'
         },
     };
-    console.log('******************');
     request(requestOption, function (err, resp, body) {
       // Insert in MongoDB values
-      const result = body.toJSON();
-      console.log('Body',result);
+      const result = JSON.parse(Buffer.from(body.toJSON()).toString('utf8'));
+
       if(result instanceof Array) {
           const dataToInsert = result.map( (entry) => {
-            console.log(entry);
-              return {sensor : entry.sensor, time : captureDate.captureDate.getNanoTime(), value : entry.value, datasource : DataSourceEnum.myfood, active : true}
+            if(moment(entry.captureDate).valueOf()>timestamp){
+              console.log('Timestamp', timestamp)
+              console.log('Moment', moment(entry.captureDate).valueOf())
+              return {sensor : entry.sensor, time : moment(entry.captureDate).valueOf(), value : entry.value, datasource : DataSourceEnum.myfood, active : true}
+            }
           });
+
           const promises = [];
           dataToInsert.forEach((row) => {
               promises.push(sensorsdata.insertPromise(row));
@@ -67,7 +72,7 @@ function myFoodTaskCron(mongoDb) {
               logger.info(`New MyFood data inserted into mongo : ${promises.length} new row since ${timestamp}`)
           });
       } else {
-          logger.error('Error MyFood Task Cron : incorrect response from influx database')
+          logger.error('Error MyFood Task Cron : incorrect response from MyFood database')
       }
     }).catch((err) => {
       console.log('Error', err)
@@ -81,7 +86,7 @@ module.exports = function (mongoDb) {
     myFoodTaskCron(mongoDb);
     cron.schedule(`${config.cron.INTERVAL} * * * * *`, () => {
         // influxTaskCron(mongoDb);
-        // myFoodTaskCron();
+        myFoodTaskCron();
     }, {
         scheduled: true,
     }).start();
